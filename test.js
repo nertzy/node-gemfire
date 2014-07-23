10447067cbe2929e0bb9b0a030853ab6e24d22b2
Grant Hutchins <pair+grant@pivotallabs.com>
2014-07-23 13:47:18 -0500

    Rename index.js to test.js

--- test.js
+++ test.js
@@ -3,4 +3,6 @@ var path = require('path')
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 var binding = require(binding_path);
 
-require('assert').equal(binding.hello(),"hello");
+var assert = require('assert');
+
+assert.equal(binding.hello(),"hello");
