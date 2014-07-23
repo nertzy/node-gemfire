244f6d0a6c0ab9996e5f8a2454dd69bec1df4fb2
Grant Hutchins <pair+grant@pivotallabs.com>
2014-07-23 14:04:45 -0500

    Add grunt build and test tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,16 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    shell: {
+      rebuild: {
+        command: 'npm install --build-from-source'
+      },
+      test: {
+        command: 'node test.js'
+      }
+    },
   });
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.registerTask('default', ['shell:rebuild', 'shell:test']);
 };
 
