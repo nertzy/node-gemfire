5666915c42965cf6e2a2fe4f7163dc6daf65ab35
Grant Hutchins <pair+grant@pivotallabs.com>
2014-07-24 14:11:09 -0500

    No need to use update-alternatives for node
    nodejs-legacy does that for us.

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,5 +4,4 @@ set -e
 sudo apt-get update
 sudo apt-get install nodejs npm nodejs-legacy
 sudo npm install -g grunt-cli
-sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
 npm install --build-from-source
