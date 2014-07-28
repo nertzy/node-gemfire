5191b3d783160e801400b43fd30b5fe07e589e57
Grant Hutchins and Taavo Smith <pair+ghutchins+taavo@pivotallabs.com>
2014-07-28 12:24:48 -0400

    Add get and put methods
    For now, they write to the "exampleRegion" region, which is created
    automatically.

--- spec/gemfire_spec.js
+++ spec/gemfire_spec.js
@@ -11,15 +11,31 @@ describe("pivotal-gemfire", function() {
     pivotalGemfire = require(pivotalGemfirePath);
   });
 
-  describe(".hello", function() {
-    it("says hello", function() {
-      expect(pivotalGemfire.hello()).toEqual("hello");
-    });
-  });
-
   describe(".version", function() {
     it("returns the version string", function() {
       expect(pivotalGemfire.version()).toEqual("7.0.2.0");
+    });
+  });
+
+  describe(".put", function() {
+    it("returns the value", function() {
+      expect(pivotalGemfire.put("foo", "42")).toEqual("42");
+    });
+  });
+
+  describe(".get", function() {
+    it("returns the values from the cache", function() {
+      pivotalGemfire.put("my key", "foo")
+      expect(pivotalGemfire.get("my key")).toEqual("foo");
+
+      pivotalGemfire.put("my key", "bar")
+      expect(pivotalGemfire.get("my key")).toEqual("bar");
+    });
+  });
+
+  describe("cleanup", function(){
+    it("is not actually a test", function(){
+      pivotalGemfire.close();
     });
   });
 });
