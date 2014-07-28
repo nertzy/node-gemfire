ff973566660d2d325ec0c74a3415e57e38a03d37
Grant Hutchins and Taavo Smith <pair+ghutchins+taavo@pivotallabs.com>
2014-07-28 12:24:48 -0400

    Add get and put methods
    For now, they write to the "exampleRegion" region, which is created
    automatically.

--- src/binding.cpp
+++ src/binding.cpp
@@ -3,23 +3,61 @@
 #include <gfcpp/CacheFactory.hpp>
 #include <nan.h>
 
-NAN_METHOD(get_hello) {
-  NanScope();
-  NanReturnValue(NanNew<v8::String>("hello"));
-}
+gemfire::CachePtr cachePtr;
+gemfire::RegionPtr regionPtr;
 
-NAN_METHOD(get_version) {
+NAN_METHOD(version) {
   NanScope();
   NanReturnValue(NanNew<v8::String>(gemfire::CacheFactory::getVersion()));
 }
 
-extern "C" {
-  static void start(v8::Handle<v8::Object> target) {
-    NanScope();
-    NODE_SET_METHOD(target, "hello", get_hello);
-    NODE_SET_METHOD(target, "version", get_version);
-  }
+NAN_METHOD(put) {
+  NanScope();
+
+  v8::String::AsciiValue key(args[0]->ToString());
+  v8::String::AsciiValue value(args[1]->ToString());
+
+  gemfire::CacheableKeyPtr keyPtr = gemfire::CacheableString::create(*key);
+  gemfire::CacheablePtr valuePtr = gemfire::CacheableString::create(*value);
+
+  regionPtr->put(keyPtr, valuePtr);
+
+  NanReturnValue(NanNew<v8::String>(*value));
 }
 
-NODE_MODULE(pivotal_gemfire, start)
+NAN_METHOD(get) {
+  NanScope();
+
+  v8::String::AsciiValue key(args[0]->ToString());
+  gemfire::CacheableKeyPtr keyPtr = gemfire::CacheableString::create(*key);
+  gemfire::CacheableStringPtr valuePtr = regionPtr->get(keyPtr);
+
+  const char* value = valuePtr->asChar();
+
+  NanReturnValue(NanNew<v8::String>(value));
+}
+
+NAN_METHOD(close) {
+  NanScope();
+
+  cachePtr->close();
+
+  NanReturnValue(NanNew<v8::Boolean>(true));
+}
+
+static void Initialize(v8::Handle<v8::Object> target) {
+  NanScope();
+
+  gemfire::CacheFactoryPtr cacheFactory = gemfire::CacheFactory::createCacheFactory();
+  cachePtr = cacheFactory->create();
+  gemfire::RegionFactoryPtr regionFactory = cachePtr->createRegionFactory(gemfire::LOCAL);
+  regionPtr = regionFactory->create("exampleRegion");
+
+  NODE_SET_METHOD(target, "version", version);
+  NODE_SET_METHOD(target, "put", put);
+  NODE_SET_METHOD(target, "get", get);
+  NODE_SET_METHOD(target, "close", close);
+}
+
+NODE_MODULE(pivotal_gemfire, Initialize)
 
