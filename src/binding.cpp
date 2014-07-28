9c0ba52ff0f47c4b4121acc6dfbdc44baaf458aa
Grant Hutchins and Taavo Smith <pair+ghutchins+taavo@pivotallabs.com>
2014-07-28 12:19:49 -0400

    Stop using namespaces to be more explicit

--- src/binding.cpp
+++ src/binding.cpp
@@ -3,21 +3,18 @@
 #include <gfcpp/CacheFactory.hpp>
 #include <nan.h>
 
-using namespace v8;
-using namespace gemfire;
-
 NAN_METHOD(get_hello) {
   NanScope();
-  NanReturnValue(NanNew<String>("hello"));
+  NanReturnValue(NanNew<v8::String>("hello"));
 }
 
 NAN_METHOD(get_version) {
   NanScope();
-  NanReturnValue(NanNew<String>(CacheFactory::getVersion()));
+  NanReturnValue(NanNew<v8::String>(gemfire::CacheFactory::getVersion()));
 }
 
 extern "C" {
-  static void start(Handle<Object> target) {
+  static void start(v8::Handle<v8::Object> target) {
     NanScope();
     NODE_SET_METHOD(target, "hello", get_hello);
     NODE_SET_METHOD(target, "version", get_version);
