e04c4a00281bfc9b45c675a146b5548e9d708d3d
Grant Hutchins and Tim Johnson <pair+ghutchins+tjohnson@pivotal.io>
2014-07-29 16:18:44 -0400

    Fix path in benchmark.js

--- benchmark/node/benchmark.js
+++ benchmark/node/benchmark.js
@@ -4,7 +4,7 @@ var microtime = require("microtime");
 var nodePreGyp = require("node-pre-gyp");
 var path = require("path");
 var pivotalGemfirePath = nodePreGyp.find(
-  path.resolve(path.join(__dirname,'../package.json'))
+  path.resolve(path.join(__dirname,'../../package.json'))
 );
 var pivotalGemfire = require(pivotalGemfirePath);
 
