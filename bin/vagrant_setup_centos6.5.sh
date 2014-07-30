26405708af61ee32c98733e9e5296b567b2ba86f
Grant Hutchins and Tim Johnson <pair+ghutchins+tjohnson@pivotal.io>
2014-07-30 10:27:43 -0400

    Use CentOS6.5
    * Install node and npm
    * Run node benchmark
    * Does not install GemFire (yet)

---
+++ bin/vagrant_setup_centos6.5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e -x
+
+echo "Setting up Centos 6.5"
+
+if ! yum -C repolist | grep epel ; then
+  sudo rpm --import https://fedoraproject.org/static/0608B895.txt
+  sudo rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+fi
+
+sudo yum -y install nodejs npm --enablerepo=epel
+
+test -e /usr/bin/grunt || npm install -g grunt-cli
+
