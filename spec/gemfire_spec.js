05b438ac9757fa2aae1d0150081cf4637ff45748
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-08-04 12:34:09 -0400

    External server-side events trigger put callbacks
    This is a proof of concept, with memory leaks and other bugs for sure

--- spec/gemfire_spec.js
+++ spec/gemfire_spec.js
@@ -1,9 +1,15 @@
+var childProcess = require('child_process');
+
 describe("pivotal-gemfire", function() {
   var gemfire;
 
   beforeEach(function() {
     gemfire = require("../gemfire.js");
     gemfire.clear();
+  });
+
+  afterEach(function(done) {
+    setTimeout(done, 0);
   });
 
   describe(".version", function() {
@@ -35,19 +41,69 @@ describe("pivotal-gemfire", function() {
   });
 
   describe(".onPut", function() {
-    it("fires the callback function when a put occurs", function() {
+    describe("for puts triggered locally", function() {
       var key = Date.now().toString();
 
-      var callback = jasmine.createSpy();
+      var callback1 = jasmine.createSpy();
+      var callback2 = jasmine.createSpy();
 
-      pivotalGemfire.onPut(callback);
-      expect(callback).not.toHaveBeenCalled();
+      beforeEach(function(done) {
+        gemfire.onPut(callback1);
+        gemfire.onPut(callback2);
+        setTimeout(done, 0);
+      });
 
-      pivotalGemfire.put(key, "bar");
-      expect(callback).toHaveBeenCalledWith(key, "bar");
+      beforeEach(function() {
+        expect(callback1).not.toHaveBeenCalled();
+        expect(callback2).not.toHaveBeenCalled();
+      });
 
-      pivotalGemfire.put(key, "baz");
-      expect(callback).toHaveBeenCalledWith(key, "baz");
+      beforeEach(function(done) {
+        gemfire.put(key, "bar");
+        setTimeout(done, 0);
+      });
+
+      beforeEach(function() {
+        expect(callback1).toHaveBeenCalledWith(key, "bar");
+        expect(callback2).toHaveBeenCalledWith(key, "bar");
+      });
+
+      beforeEach(function(done) {
+        gemfire.put(key, "baz");
+        setTimeout(done, 0);
+      });
+
+      it("fires the callback function when a put occurs", function() {
+        expect(callback1).toHaveBeenCalledWith(key, "baz");
+        expect(callback2).toHaveBeenCalledWith(key, "baz");
+      });
+    });
+
+    describe("for puts triggered externally", function() {
+      var firedCallback;
+
+      beforeEach(function(done) {
+        firedCallback = false;
+
+        gemfire.onPut(function(key, value){
+          expect([key, value]).toEqual(["async", "test"]);
+          firedCallback = true;
+
+          done();
+        });
+
+        childProcess.execFile("node", ["spec/support/external_put.js"], function(error, stdout, stderr) {
+          if(error) {
+            console.error(stderr);
+            expect(error).toBeNull();
+            done();
+          }
+        });
+      });
+
+      it("fires the callback function", function() {
+        expect(firedCallback).toBeTruthy();
+      });
     });
   });
 
