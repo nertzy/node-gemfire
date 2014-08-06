4f23878fc0b5748ce9c3c0a2b0deece1bdcabd17
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-08-06 12:32:00 -0400

    Rename gemfire variable in Jasmine spec

--- spec/gemfire_spec.js
+++ spec/gemfire_spec.js
@@ -1,36 +1,36 @@
 describe("pivotal-gemfire", function() {
-  var pivotalGemfire;
+  var gemfire;
 
   beforeEach(function() {
-    pivotalGemfire = require("../gemfire.js");
-    pivotalGemfire.clear();
+    gemfire = require("../gemfire.js");
+    gemfire.clear();
   });
 
   describe(".version", function() {
     it("returns the version string", function() {
-      expect(pivotalGemfire.version()).toEqual("7.0.2.0");
+      expect(gemfire.version()).toEqual("7.0.2.0");
     });
   });
 
   describe(".put/.get", function() {
     describe("with objects", function() {
       it("returns the value and stores the object in the cache", function() {
-        expect(pivotalGemfire.put("foo", { foo: "bar" })).toEqual({ foo: "bar" });
-        expect(pivotalGemfire.get("foo")).toEqual({ foo: "bar" });
+        expect(gemfire.put("foo", { foo: "bar" })).toEqual({ foo: "bar" });
+        expect(gemfire.get("foo")).toEqual({ foo: "bar" });
       });
     });
 
     describe("with strings", function () {
       it("returns the value and stores the object in the cache", function() {
-        expect(pivotalGemfire.put("foo", "bar")).toEqual("bar");
-        expect(pivotalGemfire.get("foo")).toEqual("bar");
+        expect(gemfire.put("foo", "bar")).toEqual("bar");
+        expect(gemfire.get("foo")).toEqual("bar");
       });
     });
   });
 
   describe("cleanup", function(){
     it("is not actually a test", function(){
-      pivotalGemfire.close();
+      gemfire.close();
     });
   });
 });
