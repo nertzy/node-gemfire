69d7a7a06b0d449708370e8848e3234178bdcef9
Grant Hutchins <pair+ghutchins@pivotal.io>
2014-08-18 14:46:17 -0500

    Stop printing stack trace noise

--- src/exceptions.cpp
+++ src/exceptions.cpp
@@ -6,8 +6,5 @@
 void ThrowGemfireException(const gemfire::Exception & e) {
   std::stringstream errorMessageStream;
   errorMessageStream << e.getName() << ": " << e.getMessage();
-
-  e.printStackTrace();
-
   NanThrowError(errorMessageStream.str().c_str());
 }
