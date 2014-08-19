838c207bc7295536eea570dc20e6a0cd7a191004
Taavo Smith <pair+tsmith@pivotallabs.com>
2014-08-19 15:01:49 -0400

    grunt console has history

--- bin/console.js
+++ bin/console.js
@@ -7,6 +7,6 @@ region = cache.getRegion("exampleRegion");
 var repl = require("repl");
 repl.start({
   prompt: "node_gemfire> ",
-  useColors: true
-});
+  terminal: true
+}).on('exit', process.exit);
 
