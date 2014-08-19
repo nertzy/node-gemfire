82e7f2d45309e7b6d8015bd9fe5b383c4567f922
Grant Hutchins and Mike Dalessio <pair+ghutchins+mdalessio@pivotal.io>
2014-08-19 11:03:57 -0400

    extract gemfireExceptionMessage helper function

--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -1,7 +1,9 @@
 #ifndef __EXCEPTIONS_HPP__
 
 #include <gfcpp/GemfireCppCache.hpp>
+#include <string>
 
+std::string gemfireExceptionMessage(const gemfire::Exception & exception);
 void ThrowGemfireException(const gemfire::Exception & e);
 
 #define __EXCEPTIONS_HPP__
