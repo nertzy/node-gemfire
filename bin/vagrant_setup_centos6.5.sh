b0004d6f5c34d09643573366b8218559b6abd23b
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-08-20 11:59:46 -0400

    Switch to 64 bit epel

--- bin/vagrant_setup_centos6.5.sh
+++ bin/vagrant_setup_centos6.5.sh
@@ -8,7 +8,7 @@ echo "Setting up Centos 6.5"
 
 if ! yum -C repolist | grep epel ; then
   sudo rpm --import https://fedoraproject.org/static/0608B895.txt
-  sudo rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+  sudo rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 fi
 
 sudo yum -y install nodejs npm --enablerepo=epel
