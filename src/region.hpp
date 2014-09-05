7d7e85312b3999bdb3a3a74cb50962401875e21b
Grant Hutchins <pair+ghutchins@pivotal.io>
2014-09-05 13:22:00 -0500

    Add region.name
    [Finishes #78232464]

--- src/region.hpp
+++ src/region.hpp
@@ -30,6 +30,7 @@ class Region : node::ObjectWrap {
     static NAN_METHOD(Get);
     static NAN_METHOD(ExecuteFunction);
     static NAN_METHOD(Inspect);
+    static NAN_GETTER(Name);
     static void AsyncGet(uv_work_t * request);
     static void AfterAsyncGet(uv_work_t * request, int status);
     static void AsyncPut(uv_work_t * request);
