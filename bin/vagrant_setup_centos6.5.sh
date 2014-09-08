784684f27c5778ebe08e6d7eae9eb740e4b63306
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-09-08 17:40:28 -0400

    Upgrade to node 0.10.31
    [Finishes #78405244]

--- bin/vagrant_setup_centos6.5.sh
+++ bin/vagrant_setup_centos6.5.sh
@@ -2,55 +2,64 @@
 S3_PREFIX="http://download.pivotal.com.s3.amazonaws.com/gemfire/8.0.0/"
 GEMFIRE_SERVER_FILENAME="pivotal-gemfire-8.0.0-48398.el6.noarch.rpm"
 NATIVE_CLIENT_FILENAME="Pivotal_GemFire_NativeClient_Linux_64bit_8000_b6169.zip"
-JAVA_RPM_URL="http://download.oracle.com/otn-pub/java/jdk/7u65-b17/jdk-7u65-linux-x64.rpm"
+JAVA_RPM_FILENAME="jdk-7u65-linux-x64.rpm"
+JAVA_RPM_URL="http://download.oracle.com/otn-pub/java/jdk/7u65-b17/$JAVA_RPM_FILENAME"
+
+NODE_VERSION="0.10.31"
+NODE_TARBALL_DIR="node-v$NODE_VERSION-linux-x64"
+NODE_TARBALL_FILENAME="${NODE_TARBALL_DIR}.tar.gz"
+NODE_TARBALL_URL="http://nodejs.org/dist/v$NODE_VERSION/$NODE_TARBALL_FILENAME"
 
 set -e
 
 echo "Setting up Centos 6.5"
 
 if ! yum -C repolist | grep epel ; then
-  sudo rpm --import https://fedoraproject.org/static/0608B895.txt
-  sudo rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+  rpm --import https://fedoraproject.org/static/0608B895.txt
+  rpm -Uvh http://download-i2.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 fi
 
-sudo yum -y install nodejs --enablerepo=epel
-sudo yum -y install gcc-c++ gdb git gtest gtest-devel unzip valgrind yum-utils yum-plugin-auto-update-debug-info.noarch
-sudo debuginfo-install -y nodejs
+yum -y install gcc-c++ gdb git gtest gtest-devel unzip valgrind yum-utils yum-plugin-auto-update-debug-info.noarch
 
 if [ ! -e /usr/bin/gemfire ]; then
   if [ ! -e /project/tmp/$GEMFIRE_SERVER_FILENAME ]; then
-    wget -O /project/tmp/$GEMFIRE_SERVER_FILENAME $S3_PREFIX$GEMFIRE_SERVER_FILENAME
+    wget --no-verbose -O /project/tmp/$GEMFIRE_SERVER_FILENAME $S3_PREFIX$GEMFIRE_SERVER_FILENAME
   fi
-  sudo -E rpm -ivh /project/tmp/$GEMFIRE_SERVER_FILENAME
+  rpm -ivh /project/tmp/$GEMFIRE_SERVER_FILENAME
 fi
 
 if [ ! -e /opt/pivotal/NativeClient_Linux_64bit_8000_b6169 ]; then
   if [ ! -e /project/tmp/$NATIVE_CLIENT_FILENAME ]; then
-    wget -O /project/tmp/$NATIVE_CLIENT_FILENAME $S3_PREFIX$NATIVE_CLIENT_FILENAME
+    wget --no-verbose -O /project/tmp/$NATIVE_CLIENT_FILENAME $S3_PREFIX$NATIVE_CLIENT_FILENAME
   fi
   cd /opt/pivotal
   unzip /project/tmp/$NATIVE_CLIENT_FILENAME
 fi
 
-if [ ! -e /usr/bin/npm ]; then
-  curl -L https://npmjs.org/install.sh | skipclean=1 sh
+if [ ! -e /usr/local/bin/node ]; then
+  if [ ! -e /project/tmp/$NODE_TARBALL_FILENAME ]; then
+    wget --no-verbose -O /project/tmp/$NODE_TARBALL_FILENAME $NODE_TARBALL_URL
+  fi
+  cd /usr/local
+  tar --strip-components 1 -xzf /project/tmp/$NODE_TARBALL_FILENAME
 fi
 
-test -e /usr/bin/grunt || npm install -g grunt-cli
+test -e /usr/local/bin/grunt || /usr/local/bin/npm install -g grunt-cli
 
 if [ ! -e /usr/bin/javac ]; then
-  rm -f /tmp/java.rpm
-  wget --no-verbose -O /tmp/java.rpm --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $JAVA_RPM_URL
-  sudo -E rpm -ivh /tmp/java.rpm
+  if [ ! -e /project/tmp/$JAVA_RPM_FILENAME ]; then
+    wget --no-verbose -O /tmp/$JAVA_RPM_FILENAME --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $JAVA_RPM_URL
+  fi
+  rpm -ivh /tmp/$JAVA_RPM_FILENAME
 fi
 
-sudo sh -c "cat > /etc/profile.d/gfcpp.sh" <<'EOF'
+sh -c "cat > /etc/profile.d/gfcpp.sh" <<'EOF'
 export GFCPP=/opt/pivotal/NativeClient_Linux_64bit_8000_b6169
 export GEMFIRE=/opt/pivotal/gemfire/Pivotal_GemFire_800
 export JAVA_HOME=/usr/java/default
-export PATH=$GFCPP/bin:$PATH
+export PATH=$GFCPP/bin:/usr/local/bin:$PATH
 export LD_LIBRARY_PATH=$GFCPP/lib:$LD_LIBRARY_PATH
 EOF
 
-sudo wget https://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py -O /usr/local/bin/cpplint.py
+sudo wget --no-verbose https://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py -O /usr/local/bin/cpplint.py
 sudo chmod +x /usr/local/bin/cpplint.py
