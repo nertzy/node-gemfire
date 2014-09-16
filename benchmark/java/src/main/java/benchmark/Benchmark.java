697f7efffe99bc9e8f8bd1082516e91a8058af52
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-09-16 17:43:18 -0400

    Benchmark OQL queries in java
    [Finishes #78832256]

--- benchmark/java/src/main/java/benchmark/Benchmark.java
+++ benchmark/java/src/main/java/benchmark/Benchmark.java
@@ -3,6 +3,7 @@ package benchmark;
 import com.gemstone.gemfire.cache.*;
 import com.gemstone.gemfire.cache.client.ClientCache;
 import com.gemstone.gemfire.cache.client.ClientCacheFactory;
+import com.gemstone.gemfire.cache.query.*;
 import com.gemstone.gemfire.pdx.JSONFormatter;
 import com.gemstone.gemfire.pdx.PdxInstance;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -14,14 +15,14 @@ import java.util.ArrayList;
 
 public class Benchmark {
     public static final int NANOSECONDS_IN_A_MILLISECOND = 1000 * 1000;
+    private static final int QUERY_COUNT = 10;
+    private static final int MICROSECONDS_IN_A_MILLISECOND = 1000;
+    private static final int MILLISECONDS_IN_A_SECOND = 1000;
     static int KEY_SIZE = 8;
     static int VALUE_SIZE = 15 * 1024;
     static int NUMBER_OF_ITEMS = 10000;
-    private static String testString;
-    private static String simpleObject;
-    private static String randomObject;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws NameResolutionException, TypeMismatchException, QueryInvocationTargetException, FunctionDomainException {
 
         ClientCache cache = new ClientCacheFactory()
                 .set("log-level", "warning")
@@ -36,8 +37,9 @@ public class Benchmark {
             throw new RuntimeException("Smoke test failed");
         }
 
-        testString = RandomStringUtils.randomAlphanumeric(VALUE_SIZE);
-        simpleObject = "{ \"foo\": \"" + testString + "\" }";
+        String testString = RandomStringUtils.randomAlphanumeric(VALUE_SIZE);
+        String simpleObject = "{ \"foo\": \"" + testString + "\" }";
+        String randomObject;
         try {
             randomObject = FileUtils.readFileToString(new File("../data/randomObject.json"));
         } catch (Exception e) {
@@ -53,21 +55,18 @@ public class Benchmark {
         benchmarkObjectPut(region, keys, simpleObject);
         benchmarkObjectPut(region, keys, randomObject);
 
-        region.clear();
-        System.out.println("Benchmarking string put...");
         ArrayList<Double> stringPutResults = benchmarkStringPut(region, keys, testString);
-
-        region.clear();
-        System.out.println("Benchmarking simple object put...");
-        ArrayList<Double> simpleObjectPutResults = benchmarkObjectPut(region, keys, simpleObject);
-
-        region.clear();
-        System.out.println("Benchmarking complex object put...");
-        ArrayList<Double> complexObjectPutResults = benchmarkObjectPut(region, keys, randomObject);
-
         writeData("String put", stringPutResults);
+
+        ArrayList<Double> simpleObjectPutResults = benchmarkObjectPut(region, keys, simpleObject);
         writeData("Simple object put", simpleObjectPutResults);
+
+        ArrayList<Double> complexObjectPutResults = benchmarkObjectPut(region, keys, randomObject);
         writeData("Complex object put", complexObjectPutResults);
+
+        benchmarkOQL(cache, 100);
+        benchmarkOQL(cache, 1000);
+        benchmarkOQL(cache, 10000);
 
         cache.close();
     }
@@ -86,6 +85,7 @@ public class Benchmark {
         ArrayList<Double> results = new ArrayList<Double>();
         Long lastNanoTime;
 
+        region.clear();
         for(Object key : keys) {
             lastNanoTime = System.nanoTime();
 
@@ -101,6 +101,7 @@ public class Benchmark {
         ArrayList<Double> results = new ArrayList<Double>();
         Long lastNanoTime;
 
+        region.clear();
         for(Object key : keys) {
             lastNanoTime = System.nanoTime();
 
@@ -114,22 +115,56 @@ public class Benchmark {
         return results;
     }
 
+    private static void benchmarkOQL(ClientCache cache, int n) throws NameResolutionException, TypeMismatchException, QueryInvocationTargetException, FunctionDomainException {
+        String needleJSON = "{\"name\":\"Jane Doe\",\"addresses\":[{\"phoneNumbers\":[{\"number\":\"212-987-5440\"},{\"number\":\"717-734-2230\"}]},{\"city\":\"New York\"}]}";
+        String haystackJSON = "{\"name\":\"Jane Doe\",\"addresses\":[{\"phoneNumbers\":[{\"number\":\"555-555-1212\"},{\"number\":\"415-77-PIVOT\"}]},{\"city\":\"New York\"}]}";
+        String query = "SELECT person.name " +
+                "FROM " +
+                "(SELECT * FROM /oqlBenchmark jr " +
+                " WHERE is_defined(jr.addresses)) person, " +
+                "(SELECT * FROM person.addresses) a " +
+                "WHERE is_defined(a.phoneNumbers) " +
+                "AND '212-987-5440' IN ( " +
+                "SELECT n.number FROM a.phoneNumbers n)";
+
+        Region region = cache.getRegion("oqlBenchmark");
+        region.clear();
+        region.put("needle", JSONFormatter.fromJSON(needleJSON));
+        for(int i = 0; i < n - 1; i++) {
+            region.put("haystack_" + i, JSONFormatter.fromJSON(haystackJSON));
+        }
+
+
+        ArrayList<Double> results = new ArrayList<Double>();
+        Long lastNanoTime;
+
+        for(int i = 0; i < QUERY_COUNT; i++) {
+            lastNanoTime = System.nanoTime();
+
+            region.getRegionService().getQueryService().newQuery(query).execute();
+
+            results.add((double)(System.nanoTime() - lastNanoTime));
+        }
+
+        writeData("OQL (" + n + " entries)", results);
+    }
+
     private static void writeData(String description, ArrayList<Double> results) {
-        double[] unboxedResults = new double[NUMBER_OF_ITEMS];
-        for(int i = 0; i < NUMBER_OF_ITEMS; i++) {
+        int numberOfItems = results.size();
+        double[] unboxedResults = new double[numberOfItems];
+        for(int i = 0; i < numberOfItems; i++) {
             unboxedResults[i] = results.get(i);
         }
 
         DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics(unboxedResults);
 
-        double mean = descriptiveStatistics.getMean() / NANOSECONDS_IN_A_MILLISECOND;
-        double standardDeviation = descriptiveStatistics.getStandardDeviation() / NANOSECONDS_IN_A_MILLISECOND;
-        double ninetyFifthPercentile = descriptiveStatistics.getPercentile(95) / NANOSECONDS_IN_A_MILLISECOND;
+        double meanMillis = descriptiveStatistics.getMean() / NANOSECONDS_IN_A_MILLISECOND;
+        double standardDeviationMillis = descriptiveStatistics.getStandardDeviation() / NANOSECONDS_IN_A_MILLISECOND;
 
-        System.out.println("\nResults for " + description + ": ");
-        System.out.format("%f operations per second\n", 1000 / mean);
-        System.out.format("Mean duration: %fms\n", mean);
-        System.out.format("Std dev: %fms\n", standardDeviation);
-        System.out.format("95th percentile: %fms\n", ninetyFifthPercentile);
+        System.out.format("%s: %d (± %d) usec/op %d ops/sec\n",
+                description,
+                Math.round(meanMillis * MICROSECONDS_IN_A_MILLISECOND),
+                Math.round(standardDeviationMillis * MICROSECONDS_IN_A_MILLISECOND),
+                Math.round(MILLISECONDS_IN_A_SECOND/meanMillis));
     }
 }
