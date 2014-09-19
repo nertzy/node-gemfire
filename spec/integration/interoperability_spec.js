f335de2b81057538d7ac55f0cf40bc25cd7fc72e
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-09-19 16:37:45 -0400

    Treat gemfire floats as javascript numbers
    [Finishes #76536898]

--- spec/integration/interoperability_spec.js
+++ spec/integration/interoperability_spec.js
@@ -4,11 +4,22 @@ const cache = require("../support/factories.js").getCache();
 const region = cache.getRegion("exampleRegion");
 
 describe("Interoperability", function() {
-  it("interprets Java null as JavaScript null", function(done) {
+  beforeEach(function() {
     this.addMatchers(errorMatchers);
+  });
+
+  it("interprets Java null as JavaScript null", function(done) {
     region.executeFunction("io.pivotal.node_gemfire.ReturnNull", function(error, response) {
       expect(error).not.toBeError();
       expect(response).toEqual([null]);
+      done();
+    });
+  });
+
+  it("interprets Java float as JavaScript Number", function(done) {
+    region.executeFunction("io.pivotal.node_gemfire.ReturnFloat", function(error, response) {
+      expect(error).not.toBeError();
+      expect(response).toEqual([1.0]);
       done();
     });
   });
