bc819834aa35d88d1829c72d6730af1ee7d980af
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-09-23 10:34:43 -0400

    Set up a default region error handler in console

--- bin/console.js
+++ bin/console.js
@@ -4,6 +4,8 @@ gemfire = require("../spec/support/gemfire.js");
 cache = new gemfire.Cache("benchmark/xml/BenchmarkClient.xml");
 region = cache.getRegion("exampleRegion");
 
+region.on('error', console.log);
+
 var repl = require("repl");
 repl.start({
   prompt: "node_gemfire> ",
