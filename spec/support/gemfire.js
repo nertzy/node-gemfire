246f3cb7a5a50b2c427f7e09bec9d7a545125ede
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-09-23 10:34:19 -0400

    Make put callback optional, emit event on failure
    [#78382720]

--- spec/support/gemfire.js
+++ spec/support/gemfire.js
@@ -1,7 +1,2 @@
-var nodePreGyp = require('node-pre-gyp');
-var path = require('path');
-var bindingPath = nodePreGyp.find(
-  path.resolve(path.join(__dirname,'../../package.json')),
-  { "debug": true }
-);
-module.exports = require(bindingPath);
+const binding = require("../../lib/binding.js");
+module.exports = binding({debug: true});
