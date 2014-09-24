fd983af8dcdbf631d64b850141bd53e12288c472
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-09-24 11:37:02 -0400

    Use nvm to manage node versions
    [#79438810]

---
+++ bin/vagrant_node.sh
@@ -0,0 +1,11 @@
+curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | bash
+source ~/.nvm/nvm.sh
+
+nvm install 0.10
+nvm install 0.11
+
+nvm exec 0.10 npm install -g grunt-cli
+nvm exec 0.11 npm install -g grunt-cli
+
+nvm alias default 0.10
+
