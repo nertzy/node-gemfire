de028519bb23389cb44825824884d2b71cf99fae
Grant Hutchins and Paul Meskers <pair+ghutchins+pmeskers@pivotal.io>
2014-10-17 15:49:22 -0400

    Bump nvm to 0.17.2

--- bin/vagrant_node.sh
+++ bin/vagrant_node.sh
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | bash
+curl https://raw.githubusercontent.com/creationix/nvm/v0.17.2/install.sh | bash
 source ~/.nvm/nvm.sh
 
 nvm install 0.10
