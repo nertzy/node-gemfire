26499ed2b8969d3558180447f4b490e1088a20b0
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2015-01-07 09:34:33 -0600

    Add v8Error() conversion function
    [#85471240]

--- src/exceptions.hpp
+++ src/exceptions.hpp
@@ -2,11 +2,13 @@
 #define __EXCEPTIONS_HPP__
 
 #include <gfcpp/GemfireCppCache.hpp>
+#include <v8.h>
 #include <string>
 
 namespace node_gemfire {
 
 std::string gemfireExceptionMessage(const gemfire::Exception & exception);
+v8::Handle<v8::Value> v8Error(const gemfire::Exception & exception);
 std::string gemfireExceptionMessage(gemfire::UserFunctionExecutionExceptionPtr exceptionPtr);
 void ThrowGemfireException(const gemfire::Exception & e);
 
