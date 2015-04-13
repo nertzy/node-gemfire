55a5d01d243befedad81bc56d01ab382c829af20
Michael Schubert <mschubert@pivotal.io>
2015-04-13 07:29:31 +0000

    Fix v0.11 node installer and add in v0.12

--- bin/vagrant_node.sh
+++ bin/vagrant_node.sh
@@ -3,10 +3,13 @@ if ! [ -e ~/.nvm/nvm.sh ]; then
 fi
 source ~/.nvm/nvm.sh
 
-nvm list 0.10 || nvm install 0.10
+nvm list 0.10 || nvm install -s 0.10
 nvm exec 0.10 which grunt || nvm exec 0.10 npm install -g grunt-cli jasmine
 
-nvm list 0.11 || nvm install 0.11
+nvm list 0.11 || nvm install -s 0.11
 nvm exec 0.11 which grunt || nvm exec 0.11 npm install -g grunt-cli jasmine
+
+nvm list 0.12 || nvm install -s 0.12
+nvm exec 0.12 which grunt || nvm exec 0.12 npm install -g grunt-cli jasmine
 
 nvm alias default 0.10
